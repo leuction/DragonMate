0f88c827be6a17913231bcfb24045b5b70c74ef3
leuction <unconfigured@null.spigotmc.org>
2015-08-03 18:33:28 +0800

    写了一些注释

--- app/src/main/java/com/example/shuowang/dragonmate/QuestionnaireFragment.java
+++ app/src/main/java/com/example/shuowang/dragonmate/QuestionnaireFragment.java
@@ -15,6 +15,16 @@ public class QuestionnaireFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_questionnaire,container,false);
+
+        //这个就是需要写的Fragment,对应的是fragment_questionnaire.xml布局，基本要写的东西参考一下accountSettingFragment就可以了，就是那个bmobUpdate方法。
+        //需要添加属性的话在MyUser里添加就可以了。记得写get和set方法。
+
+
+
+
+
+
+
         return root;
     }
 }
